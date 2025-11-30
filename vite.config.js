06a5a140e2b7fe226570a1b4dd73521f0003f304
yuvraj singh bhadoria <yuvrajsinghbhado2030@gmail.com>
2025-11-30 17:17:32 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Weather-Dashboard/',   // FIXED: Always set repo name
+  base: process.env.NODE_ENV === 'production' ? '/Weather-Dashboard/' : '/',
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
+            // Split vendor dependencies into separate chunks
             return id.toString().split('node_modules/')[1].split('/')[0];
           }
         },
       },
     },
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 1000, // Increase the limit if necessary
   },
 });
